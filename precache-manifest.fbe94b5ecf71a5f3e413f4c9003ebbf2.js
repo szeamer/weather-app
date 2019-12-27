57e09a5ef913aa3b652c4e6cac394832611072a4
Silvia Zeamer <szeamer@wellesley.edu>
2019-12-26 22:14:58 -0500

    Updates

---
+++ precache-manifest.fbe94b5ecf71a5f3e413f4c9003ebbf2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79d3af3eb132cc6eda660494d3b84c5c",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "94ba1e7236c0480189a0",
+    "url": "/weather-app/static/css/main.c156cb45.chunk.css"
+  },
+  {
+    "revision": "fbdc9278402404b2e227",
+    "url": "/weather-app/static/js/2.01967e6f.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/weather-app/static/js/2.01967e6f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "94ba1e7236c0480189a0",
+    "url": "/weather-app/static/js/main.c0fc94e0.chunk.js"
+  },
+  {
+    "revision": "df1ac26dc0e7b7d18b20",
+    "url": "/weather-app/static/js/runtime-main.a7aa11b4.js"
+  }
+]);
